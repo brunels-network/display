856807e288f70dff563ff87537becd4da559f8dd
Christopher Woods <chryswoods@hey.com>
2021-09-08 10:57:07 +0100

    Added space for the credits

--- src/components/ImageOverlay.js
+++ src/components/ImageOverlay.js
@@ -1,3 +1,4 @@
+import { style } from "d3";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -7,11 +8,13 @@ import styles from "./ImageOverlay.module.css";
 
 function ImageOverlay(props) {
   let filename = props.image;
+  let credits = "Credits for the image";
 
   return (
     <div className={styles.container} onClick={props.close}>
       <img className={styles.image}
            src={require(`../${filename}`)} alt="A document" />
+      <div className={styles.credits}>{credits}</div>
     </div>
   );
 }
